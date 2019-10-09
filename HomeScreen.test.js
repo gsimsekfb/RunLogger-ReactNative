20478f917b7b4e7fb2e922a53f7662ea91222476
Gokhan Simsek <gokhansimsekfb@gmail.com>
2019-10-09 16:55:08 +0300

    Adding testing async functions and objects

--- HomeScreen.test.js
+++ HomeScreen.test.js
@@ -1,15 +1,51 @@
 
 import { when } from 'jest-when'
 import {getMonthAndYear} from './HomeScreen';
+import Axios from 'axios';
 
-// 1
+// 1. Most basic test 
 test('getMonthAndYear()', () => {
   const monthAndYear = getMonthAndYear(new Date('December 17, 1995 03:24:00'));
   expect(monthAndYear).toBe('12.1995');
 })
 
+// 2. Object test
+const user1 = {name: 'xx'};
+const user2 = {name: 'xx'};
 
-// 2.a Mocking Add function calls/returns using jest-when
+test('users', () => {
+  // expect(user1).toBe(user2);    // Use toBe with primitive types, 
+  expect(user1).toStrictEqual(user2);
+})
+
+// 3. Async test
+function fetchUser() {
+  return Axios.get('https://jsonplaceholder.typicode.com/users/1')
+       .then(res => res.data) 
+       .catch(err => console.log('fetchUser() err: ' + err));
+}
+
+async function fetchUser2() {  
+  try {
+    const res = await Axios.get('https://jsonplaceholder.typicode.com/users/1');       
+    return res.data.name;
+  }
+  catch(err) {
+    console.log('fetchUser2() err: ' + err);
+    return(null);
+  }  
+}
+
+test('Async test', async () => {  
+  // expect.assertions(1);  // is it needed ??
+  expect(await fetchUser2()).toBe('Leanne Graham');
+  return fetchUser().then(data => expect(data.name).toBe('Leanne Graham'));
+});
+
+
+// ------------------- Mock -----------------------
+
+// 1.a Mocking Add function calls/returns using jest-when
 // npm i --save-dev jest-when
 const _mockAdd = jest.fn()
 when(_mockAdd).calledWith(1,2).mockReturnValue(3)
@@ -21,7 +57,7 @@ test('mockAdd-a', () => {
 });
 
 
-// 2.b Mocking Add function calls/returns by hand
+// 1.b Mocking Add function calls/returns by hand
 test('mockAdd-b', () => {
   mockDataForAdd.map(
     e => expect(mockAdd(e.x, e.y)).toBe(e.result)
@@ -51,4 +87,4 @@ jest.mock('react-native-gesture-handler', () => null);
 jest.mock('react-navigation-stack', () => null);
 jest.mock('@react-native-community/async-storage', () => null);
 jest.mock('react-native-device-info', () => null);    
-jest.mock('react-native-fs', () => () => null); // <--- :)
+jest.mock('react-native-fs', () => () => null); // <--- ?? wth :)
